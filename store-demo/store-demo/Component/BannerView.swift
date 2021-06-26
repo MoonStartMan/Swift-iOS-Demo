b1c6661331680408b6aac6b04714a2f91a47524b
MoonStarMan <974556594@qq.com>
2021-06-26 17:54:00 +0800

    update swift-ios-demo

--- store-demo/store-demo/Component/BannerView.swift
+++ store-demo/store-demo/Component/BannerView.swift
@@ -11,21 +11,31 @@ import SnapKit
 
 protocol BannerViewDataSource: AnyObject {
     func numberOfBanners(_ bannerView: BannerView) -> Int
-    func ViewForBanner(_ bannerView: BannerView, index: Int, convertView: UIView) -> UIView
+    func ViewForBanner(_ bannerView: BannerView, index: Int, convertView: UIView?) -> UIView
 }
 
 protocol BannerViewDelegate: AnyObject {
     func didSelectBanner(_ bannerView: BannerView, index: Int)
 }
 
-class BannerView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
+class BannerView: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     
-    var collectionView: UICollectionView?
-    var flowLayout: UICollectionViewFlowLayout?
+    var collectionView: UICollectionView
+    var flowLayout: UICollectionViewFlowLayout
     
-    var pageControl: UIPageControl?
+    var pageControl: UIPageControl
     
-    weak var dataSource: BannerViewDataSource!
+    weak var dataSource: BannerViewDataSource! {
+        didSet {
+            pageControl.numberOfPages = self.dataSource.numberOfBanners(self)
+            collectionView.reloadData()
+            if isInfinite {
+                DispatchQueue.main.async {
+                    self.collectionView.setContentOffset(CGPoint(x: self.collectionView.frame.width, y: 0), animated: false)
+                }
+            }
+        }
+    }
     weak var delegate: BannerViewDelegate?
     
     var autoScrollInterval: Float = 0
@@ -35,40 +45,41 @@ class BannerView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
     var timer: Timer?
     
     static var cellID = "bannerViewCellID"
+    static var convertViewTag = 10086
     
     override init(frame: CGRect) {
-        super.init(frame: frame)
         flowLayout = UICollectionViewFlowLayout()
-        flowLayout?.headerReferenceSize = .zero
-        flowLayout?.footerReferenceSize = .zero
-        flowLayout?.minimumLineSpacing = 0
-        flowLayout?.minimumInteritemSpacing = 0
-        flowLayout?.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        flowLayout?.scrollDirection = .horizontal
+        flowLayout.headerReferenceSize = .zero
+        flowLayout.footerReferenceSize = .zero
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        flowLayout.scrollDirection = .horizontal
         
-        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height), collectionViewLayout: flowLayout!)
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height), collectionViewLayout: flowLayout)
         
         pageControl = UIPageControl()
+        super.init(frame: frame)
         self.setupViews()
     }
     
     func setupViews() {
-        collectionView?.backgroundColor = .white
-        collectionView?.isPagingEnabled = true
-        collectionView?.showsHorizontalScrollIndicator = false
-        collectionView?.contentInsetAdjustmentBehavior = .never
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: BannerView.cellID)
-        collectionView?.dataSource = self
-        collectionView?.delegate = self
+        collectionView.backgroundColor = .white
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: BannerView.cellID)
+        collectionView.dataSource = self
+        collectionView.delegate = self
         
-        self.addSubview(collectionView!)
-        self.addSubview(pageControl!)
+        self.addSubview(collectionView)
+        self.addSubview(pageControl)
         
-        collectionView?.snp.makeConstraints({ make in
+        collectionView.snp.makeConstraints({ make in
             make.edges.equalToSuperview()
         })
         
-        pageControl?.snp.makeConstraints({ make in
+        pageControl.snp.makeConstraints({ make in
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview().offset(-15)
         })
@@ -99,10 +110,93 @@ class BannerView: UIView, UICollectionViewDataSource, UICollectionViewDelegate {
             if pageNumber > 1 {
                 if indexPath.row == 0 {
                     index = pageNumber - 1
+                } else if indexPath.row == pageNumber + 1{
+                    index = 0
+                } else {
+                    index = indexPath.row - 1
                 }
             }
         }
+        
+        if let view = cell.contentView.viewWithTag(BannerView.convertViewTag) {
+            let _ = dataSource.ViewForBanner(self, index: index, convertView: view)
+        } else {
+            let newView = dataSource.ViewForBanner(self, index: index, convertView: nil)
+            newView.tag = BannerView.convertViewTag
+            cell.contentView.addSubview(newView)
+            newView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+        }
+        return cell
     }
     
+    //  开始自动轮播
+    func startAutoScroll() {
+        guard autoScrollInterval > 0 && timer == nil else {
+            return
+        }
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(autoScrollInterval), target: self, selector: #selector(filpNext), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: .common)
+    }
+    
+    //  停止滑动
+    func stopAutoScroll() {
+        if let t = timer {
+            t.invalidate()
+            timer = nil
+        }
+    }
+    
+    //  滚动到下一页
+    @objc func filpNext() {
+        guard let _ = superview, let _ = window else {
+            return
+        }
+        
+        let totalPageNumber = dataSource.numberOfBanners(self)
+        guard totalPageNumber > 1 else {
+            return
+        }
+        
+        let currentPageNumber = Int(round(collectionView.contentOffset.x / collectionView.frame.width))
+        
+        if isInfinite {
+            let nextPageNumber = currentPageNumber + 1
+            collectionView.setContentOffset(CGPoint(x: collectionView.frame.width * CGFloat(nextPageNumber), y: 0), animated: true)
+            if nextPageNumber >= totalPageNumber {
+                pageControl.currentPage = 0
+            } else {
+                pageControl.currentPage = nextPageNumber
+            }
+        } else {
+            var nextPageNumber = currentPageNumber + 1
+            if nextPageNumber >= totalPageNumber {
+                nextPageNumber = 0
+            }
+            collectionView.setContentOffset(CGPoint(x: collectionView.frame.width * CGFloat(nextPageNumber), y: 0), animated: true)
+            pageControl.currentPage = nextPageNumber
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return self.bounds.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        let total = dataSource.numberOfBanners(self)
+        let current =  Int(round(collectionView.contentOffset.x / collectionView.frame.width))
+        if current >= total + 1{
+            collectionView.setContentOffset(CGPoint(x: collectionView.frame.width, y: 0), animated: false)
+        }
+    }
     
 }
